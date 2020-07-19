58f3c92483330421aec82d953e8bc7547fa35173
Filipe Estacio <f.estacio@gmail.com>
2020-07-19 07:45:44 +0100

    added FCC react test component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import QuoteBox from './QuoteBox';
+import ReactFCCtest from 'react-fcctest';
 
 function App() {
   return (
     <div className="App">
+      <ReactFCCtest />
       <QuoteBox />
     </div>
   );
